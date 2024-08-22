69c59b0f1e2dbe4f5842f70f399ebfb651c3334a
ramontrndd <ramonbraintrindade@gmail.com>
2024-08-22 13:45:56 -0300

    feat: Update environment configuration for development

---
+++ src/shared/global-constants.ts
@@ -0,0 +1,7 @@
+export class GlobalConstants {
+  public static readonly genericError: string =
+    'An error occurred. Please try again later.';
+
+  public static readonly emailRegex: string =
+    '[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}';
+}
